aa6b347bf844fbd20dbc633fd4cfa1a01509f42b
Kiran Solanki <kirubeezy10@gmail.com>
2025-03-09 14:16:31 +0530

    Add files via upload

---
+++ rag.py
@@ -0,0 +1,112 @@
+from uuid import uuid4
+
+from dotenv import load_dotenv
+from pathlib import Path
+from langchain.chains import RetrievalQAWithSourcesChain
+from langchain_community.document_loaders import UnstructuredURLLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+from langchain_groq import ChatGroq
+from langchain_huggingface.embeddings import HuggingFaceEmbeddings
+from huggingface_hub import login
+import os
+
+CHUNK_SIZE=1000
+EMBEDDING_MODEL="Alibaba-NLP/gte-base-en-v1.5"
+COLLECTION_NAME="real_estate"
+VECTOR_STORE_DIR=Path(__file__).parent/"resources/vector_store"
+hf_token = os.getenv("HUGGINGFACEHUB_API_TOKEN")
+
+#ensure token is set
+if not hf_token:
+    raise ValueError("Hugging face api token not found .set HUGINGFACEHUB_API_TOKEN in .env or system environment")
+
+#auntenticate before using the model
+login(token=hf_token)
+
+llm=None
+vector_store=None
+
+def initialize_components():
+    global llm,vector_store
+
+    if llm is None:
+        llm=ChatGroq(model="llama-3.3-70b-versatile",temperature=0.9, max_tokens=500)
+
+    if vector_store is None:
+        ef= HuggingFaceEmbeddings(
+            model_name=EMBEDDING_MODEL,
+            model_kwargs={"trust_remote_code":True}
+        )
+
+        vector_store=Chroma(
+            collection_name=COLLECTION_NAME,
+            embedding_function=ef,
+            persist_directory=str(VECTOR_STORE_DIR)
+
+        )
+
+
+def process_urls(urls):
+    """
+    This function scraps data from the urls and stores it in a vector db
+    :param urls: input urls
+    :return
+    """
+
+    # print("Initialize components")
+    yield "Initializing components...✅"
+    initialize_components()
+    vector_store.reset_collection()
+
+    # print("load data")
+    yield "Resetting vector store...✅"
+    loader=UnstructuredURLLoader(urls=urls)
+    data=loader.load()
+
+    # print("Split Text")
+    yield "Splitting text into chunks...✅"
+    text_splitter=RecursiveCharacterTextSplitter(
+        separators=["\n\n","\n","."," "],
+        chunk_size=CHUNK_SIZE
+    )
+
+
+    docs=text_splitter.split_documents(data)
+
+    # print("Add docs to vector db")
+    yield "Adding chunks to vector database...✅"
+    uuids=[str(uuid4()) for _ in range(len(docs))]
+    vector_store.add_documents(docs,ids=uuids)
+    
+    yield "Done adding docs to vector database...✅"
+
+def generate_answer(query):
+    if not vector_store:
+        raise RuntimeError("Vector Database is not initialized")
+    
+    chain=RetrievalQAWithSourcesChain.from_llm(llm=llm,retriever=vector_store.as_retriever())
+    result=chain.invoke({"question":query}, return_only_outputs=True)
+    sources=result.get("sources","")
+
+    return result['answer'],sources
+
+if __name__=="__main__":
+    urls=[
+        # "https://www.cnbc.com/2024/12/21/how-the-federal-reserves-rate-policy-affects-mortgages.html",
+        # "https://www.cnbc.com/2024/12/20/why-mortgage-rates-jumped-despite-fed-interest-rate-cut.html"
+        "https://www.foxbusiness.com/personal-finance/todays-mortgage-rates-august-14-2024",
+        "https://www.foxbusiness.com/personal-finance/todays-mortgage-rates-august-13-2024"
+    ]  #This will process the urls
+
+    process_urls(urls)
+
+    # results=vector_store.similarity_search(
+    #     "30 year mortgage rate",
+    #     k=2
+    # )
+    # print(results)
+
+    answer,sources=generate_answer("Tell me what was the 30 year fixed mortgate rate along with the date?")
+    print(f"Answer:{answer}")
+    print(f"sources:{sources}")
